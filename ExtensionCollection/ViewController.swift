68665b81b513756ed8606335f04f6108d933a975
victory1908 <victory1908@gmail.com>
2018-10-02 22:27:53 +0800

    Initial Commit

---
+++ ExtensionCollection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExtensionCollection
+//
+//  Created by victory1908 on 2/10/18.
+//  Copyright © 2018 victory1908. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
